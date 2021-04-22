87ad3d02888b656f19d47a373609d395c8d1b14e
Jonathan Watson <thejmdw@gmail.com>
2021-04-22 00:08:34 -0400

    added the pet click function

--- scripts/RegisteredPets.js
+++ scripts/RegisteredPets.js
@@ -1,12 +1,33 @@
-import { getPets } from "./database.js"
+import { getPets, getWalkers } from "./database.js"
+
+
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const itemClicked = clickEvent.target
+        if (itemClicked.id.startsWith("pet")) {
+            const [,petId] = itemClicked.id.split("--")
+
+            const pet = pets[petId]
+
+            for (const walker of walkers) {
+                if (walker.id === parseInt(pet.walkerId)) {
+                    window.alert(`${pet.name} is being walked by ${walker.name}`)
+                }
+            }
+        }
+    }
+)
+
 
 const pets = getPets()
+const walkers = getWalkers()
 
 export const RegisteredPets = () => {
     let petHTML = "<ul>"
 
     for (const pet of pets) {
-        petHTML += `<li>${pet.name}</li>`
+        petHTML += `<li id="pet--${pet.id}">${pet.name}</li>`
     }
 
     petHTML += "</ul>"
